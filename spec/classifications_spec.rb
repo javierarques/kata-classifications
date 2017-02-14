df7106d859dba6de7399e30e4d1e3c85f3732e1a
Javi Arques <javier.arques@flywire.com>
2017-02-14 08:52:15 +0100

    Add mock request with vcr cassettes

--- spec/classifications_spec.rb
+++ spec/classifications_spec.rb
@@ -8,6 +8,13 @@ require_relative '../presenter'
 require_relative '../sorter'
 require_relative '../fetcher'
 
+require 'vcr'
+
+VCR.configure do |config|
+  config.cassette_library_dir = "fixtures/vcr_cassettes"
+  config.hook_into :webmock # or :fakeweb
+end
+
 describe 'Classification kata' do
   it 'get the JSON parsed' do
     # #Arrange
@@ -66,12 +73,14 @@ describe 'Classification kata' do
   end
 
   it 'gets some json from an url' do
-    url = 'https://classification-kata.herokuapp.com/'
-    fetcher = Fetcher.new(url)
-    some_json = '{"classification":[{"name":"Madrid","points":5},{"name":"Valencia","points":10},{"name":"Barcelona","points":7},{"name":"Zaragoza","points":8},{"name":"Bilbao","points":9}]}'
+    VCR.use_cassette("fetcher") do
+      url = 'https://classification-kata.herokuapp.com/'
+      fetcher = Fetcher.new(url)
+      some_json = '{"classification":[{"name":"Madrid","points":5},{"name":"Valencia","points":10},{"name":"Barcelona","points":7},{"name":"Zaragoza","points":8},{"name":"Bilbao","points":9}]}'
 
-    result = fetcher.call
+      result = fetcher.call
 
-    expect(result).to eq(some_json)
+      expect(result).to eq(some_json)
+    end
   end
 end
